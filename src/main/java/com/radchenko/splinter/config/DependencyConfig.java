29a90fa086473be950620b7634a1f20e8b916d2c
vradchenko <vitaliy.radchenko2@dataart.com>
2019-01-31 15:32:26 +0200

    rename AppConfig to DependencyConfig

--- src/main/java/com/radchenko/splinter/config/DependencyConfig.java
+++ src/main/java/com/radchenko/splinter/config/DependencyConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class AppConfig {
+public class DependencyConfig {
 
     @Bean
     public ModelMapper modelMapper() {
