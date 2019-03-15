b025e0195804aa45916517511af7da1d54c7e231
vradchenko <vitaliy.radchenko2@dataart.com>
2019-03-15 14:56:27 +0200

    postgress prop add

--- src/main/java/com/radchenko/splinter/config/WebSecurityConfig.java
+++ src/main/java/com/radchenko/splinter/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.radchenko.splinter.config;
 
-import com.radchenko.splinter.entity.role.RoleType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/radchenko/splinter/service/auth/UserPrincipal.java
+++ src/main/java/com/radchenko/splinter/service/auth/UserPrincipal.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
